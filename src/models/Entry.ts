8bd23076152025de36bf518bd095290b88b539f1
Williams Mata <williams.rm99@gmail.com>
2023-04-13 14:19:52 -0400

    Model Entry created

---
+++ src/models/Entry.ts
@@ -0,0 +1,26 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { Entry } from "@/interfaces";
+
+interface IEntry extends Entry {}
+
+const entrySchema = new Schema({
+  description: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Number,
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ["pending", "in-progress", "finished"],
+      message: "{VALUE} is not an allowed state",
+    },
+  },
+});
+
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model("Entry", entrySchema);
+
+export default EntryModel;

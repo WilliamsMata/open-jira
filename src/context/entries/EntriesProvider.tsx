ed303650fd3d80548d4ccba7c7974045ad7c02e1
Williams Mata <williams.rm99@gmail.com>
2023-04-14 17:16:55 -0400

    edit entry with drag and drop done

--- src/context/entries/EntriesProvider.tsx
+++ src/context/entries/EntriesProvider.tsx
@@ -20,8 +20,17 @@ export const EntriesProvider = ({ children }: PropsWithChildren) => {
     dispatch({ type: "[Entries] - Add-Entry", payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: "[Entries] - Entry-Updated", payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      });
+
+      dispatch({ type: "[Entries] - Entry-Updated", payload: data });
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
   const refreshEntries = async () => {
